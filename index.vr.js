d347a91a78a0bece7c2def09fe7cc7622b172bb4
Ernesto R. C. Pereda <ercp42@gmail.com>
2017-05-09 09:44:56 -0300

    Insert models

--- index.vr.js
+++ index.vr.js
@@ -6,27 +6,51 @@ import {
   Pano,
   Text,
   View,
+  Model,
+  AmbientLight,
 } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('venice.jpg')}/>
-        <Text
+        <Pano source={asset('sky.jpg')}/>
+
+        <Model 
+          source={{ obj: asset('huerto.obj'), mtl: asset('huerto.mtl') }} 
+          lit={ true } 
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -6]}],
-          }}>
-          Welcome to Ingenious
-        </Text>
+            transform: [
+              { translate: [0, -20, 0] }
+            ],
+          }}
+        />
+        <Model 
+          source={{ obj: asset('zanahoria.obj'), mtl: asset('zanahoria.mtl') }}
+          lit={ true }
+          style={{
+            transform: [
+              { translate: [0, 0, -5] }
+            ],
+          }}
+        />
+        <AmbientLight intensity={ 2.6 } />
+
+        { //<Text
+        //  style={{
+        //    backgroundColor: '#777879',
+        //    fontSize: 0.8,
+        //    fontWeight: '400',
+        //    layoutOrigin: [0.5, 0.5],
+        //    paddingLeft: 0.2,
+        //    paddingRight: 0.2,
+        //    textAlign: 'center',
+        //    textAlignVertical: 'center',
+        //    transform: [{translate: [0, 0, -6]}],
+        //  }}>
+        //  Welcome to Ingenious
+        //</Text>
+        }
       </View>
     );
   }
